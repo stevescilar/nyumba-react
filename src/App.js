96c7f30cbf40dfa123d02778338f4ee8def903a1
stevescilar <stevemuambi@gmail.com>
2023-01-23 16:03:27 +0300

    initial Commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <>
+      <h1>Hello Steve! We are in for a Code Journey</h1>
+    </>
+  );
+}
+
+export default App;
