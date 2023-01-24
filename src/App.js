5b7356cf05d2f09d47a1edc000476924853a1957
stevescilar <stevemuambi@gmail.com>
2023-01-24 09:44:46 +0300

    Few Editing

--- src/App.js
+++ src/App.js
@@ -1,10 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
-
 function App() {
   return (
     <>
-      <h1 className="text-2xl bg-green-400">Great</h1>
+      <h1>Great</h1>
     </>
   );
 }
