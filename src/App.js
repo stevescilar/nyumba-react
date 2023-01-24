5cc6a07911de13208d8fbcb1f86dbd313f19d00e
stevescilar <stevemuambi@gmail.com>
2023-01-24 10:45:12 +0300

    setting some routes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import ForgotPassword from "./pages/ForgotPassword";
+import Home from "./pages/Home";
+import Offers from "./pages/Offers";
+import Profile from "./pages/Profile";
+import SignIn from "./pages/SignIn";
+import SignUp from "./pages/SignUp";
+
+
 function App() {
   return (
     <>
-      <h1>Great</h1>
+      <Router>
+        <Routes>
+          <Route path="/forgot-password" element={<ForgotPassword/>}></Route>
+          <Route path="/" element={<Home/>}></Route>
+          <Route path="/offers" element={<Offers/>}></Route>
+          <Route path="/profile" element={<Profile/>}></Route>
+          <Route path="/sign-in" element={<SignIn/>}></Route>
+          <Route path="/sign-out" element={<SignUp/>}></Route>
+        </Routes>
+      </Router>
     </>
   );
 }
