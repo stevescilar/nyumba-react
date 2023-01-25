f5d73a604a898e0fac1b4f26559762bd7368eb44
stevescilar <stevemuambi@gmail.com>
2023-01-25 23:09:18 +0300

    update-nav

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,17 @@ import React from 'react'
 export default function Header() {
   return (
     <div>
-        <header>
+        <header className='flex justify-between items-center px-3 max-w-6xl'>
           
             <div>
                 <img src={"./Gadgetify.png"} alt="logo"  width={150} class="cursor-pointer" />
+            </div>
+            <div>
+              <ul>
+                <li>Home</li>
+                <li>Offers</li>
+                <li>Contact</li>
+              </ul>
             </div>
         </header>
     </div>
