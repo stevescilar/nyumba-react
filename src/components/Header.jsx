db01f17ba24c2440d8b7ce4a83533a398135b46b
stevescilar <stevemuambi@gmail.com>
2023-01-25 22:56:07 +0300

    update-tailwind

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <div>
         <header>
-          {/* working for the mix */}
+          
             <div>
                 <img src={"./Gadgetify.png"} alt="logo"  width={150} class="cursor-pointer" />
             </div>
